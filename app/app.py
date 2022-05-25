9c1ded0b341ad4c94982a690d6ce1dd7f153f8fa
Ihor Mozil <mozil@energyhub.net>
2022-05-26 01:17:12 +0300

    kiev datetime, added styles

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 
 from os import environ
 from flask import Flask, request, render_template
@@ -11,7 +12,7 @@ app_version = environ.get("APP_VERSION", default="v1")
 def get_ip():
     return render_template(
         "index.html",
-        utc_dt=datetime.datetime.utcnow(),
+        now_dt=datetime.datetime.now((pytz.timezone("Europe/Kiev"))),
         ip_address=request.remote_addr,
         version=app_version,
     )
