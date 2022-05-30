242acdad18d30cc2120d25571996423883449371
Ihor Mozil <mozil@energyhub.net>
2022-05-30 17:27:54 +0300

    added Power kWh demo data

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 import datetime
-import pytz
-
+import random
 from os import environ
-from flask import Flask, request, render_template
+
+import pytz
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app_version = environ.get("APP_VERSION", default="v1")
@@ -13,6 +14,7 @@ def get_ip():
     return render_template(
         "index.html",
         now_dt=datetime.datetime.now((pytz.timezone("Europe/Kiev"))),
+        kwh_data=round(random.uniform(1,5), 3),
         ip_address=request.remote_addr,
         version=app_version,
     )
